0f94111dc40d3a6c0c400c3aae081323c90d124e
variyaadi_1061 <variyaadi5@gmail.com>
2025-10-07 19:25:57 +0530

    Add files via upload

---
+++ ass_1061_9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float fahrenheit, celsius;
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &fahrenheit);
+
+    celsius = (fahrenheit - 32) * 5 / 9;
+    printf("%.2f Fahrenheit = %.2f Celsius\n", fahrenheit, celsius);
+    return 0;
+}
+
